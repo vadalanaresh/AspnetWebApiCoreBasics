7822b78c51b1ad0f4850b21dfb26405722ffc638
Naresh Vadala <naresh.vadala@questionmark.com>
2020-05-13 19:17:29 +0530

    Module3 demos

---
+++ CouresesLibrary.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouresesLibrary.Api.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authors = _courseLibraryRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthors(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+            if (author == null) return NotFound();
+            return Ok(author);
+        }
+    }
+}
